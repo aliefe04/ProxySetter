37f10d5f1c494c2efcdee6c54db485bc835691b3
aliefe04 <aliefe.cakici@std.yeditepe.edu.tr>
2023-01-10 14:45:42 +0300

    ADDED GUI

--- main.py
+++ main.py
@@ -1,23 +1,59 @@
 import win32api
 import subprocess
-import sys
+import tkinter
+import customtkinter
 
 proxy_server_querys = 'reg query "HKCU\Software\Micro   soft\Windows\CurrentVersion\Internet Settings" /v ProxyServer'
 proxy_status_querys = 'reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable'
 deactivate_proxys = 'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f'
 activate_proxys = 'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f'
 
-win32api.ShellExecute(0, "open", "cmd.exe", "/c " + deactivate_proxys, "", 0)
 
+def check_active_proxy():
+    regkey_check = subprocess.Popen(
+        proxy_status_querys, shell=True, stdout=subprocess.PIPE
+    )
+    regkey_check_return = regkey_check.stdout.read().split()
+
+    if regkey_check_return[-1] == b"0x0":
+        return "Proxy is currently inactive"
+    if regkey_check_return[-1] == b"0x1":
+        return "Proxy is currently active"
+    else:
+        print(f"{regkey_check_return[-1]}, {type(regkey_check_return[-1])}")
+
+
+def check_proxy_address():
+    try:
+        result = subprocess.run(
+            'reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer',
+            shell=True,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        stdout = result.stdout.decode()
+        proxy_address = stdout.strip().split('    ')[-1]
+        return proxy_address
+    except subprocess.CalledProcessError:
+        return None
 
 def activate_proxy():
     win32api.ShellExecute(0, "open", "cmd.exe", "/c " + activate_proxys, "", 0)
-    print("Proxy activated")
+    if activeproxy.winfo_exists() == 1:
+        activeproxy.destroy()
+    customtkinter.CTkLabel(master=app, text="Proxy activated").place(
+        relx=0.05, rely=0.4, anchor=tkinter.NW
+    )
+
 
 def deactivate_proxy():
     win32api.ShellExecute(0, "open", "cmd.exe", "/c " + deactivate_proxys, "", 0)
-    print("Proxy deactivated")
-
+    if activeproxy.winfo_exists() == 1:
+        activeproxy.destroy()
+    customtkinter.CTkLabel(master=app, text="Proxy deactivated").place(
+        relx=0.05, rely=0.4, anchor=tkinter.NW
+    )
 
 def change_address(new_address):
     win32api.ShellExecute(
@@ -29,50 +65,34 @@ def change_address(new_address):
         "",
         0,
     )
-    print(f"Changed Proxy Server to {new_address}")
+    i = customtkinter.CTkLabel(master=app, text=f"Changed Proxy Server to {new_address}")
+    i.place(
+         relx=0.05, rely=0.5, anchor=tkinter.NW)
+    i.after(3000, i.destroy)
 
 
-# returns latest proxy server address
-def fill_in():
-    try:
-        value = subprocess.check_output(proxy_server_querys).decode("utf-8").split()[-1]
-        return value
-    except:
-        value = "0.0.0.0:0"
-        return value
-
-
-def status_check():
-    # check current regkey value for proxy
-    regkey_check = subprocess.Popen(
-        proxy_status_querys, shell=True, stdout=subprocess.PIPE
-    )
-    regkey_check_return = regkey_check.stdout.read().split()
-
-    if regkey_check_return[-1] == b"0x0":
-        print("Proxy is currently inactive")
-        return
-    if regkey_check_return[-1] == b"0x1":
-        print("Proxy is currently active")
-        return
+def button_click_event():
+    dialog = customtkinter.CTkInputDialog(text=f"Type new proxy\n current proxy is:\n {check_proxy_address()} ", title="Change Proxy")
+    response = dialog.get_input()
+    if response == None:
+        pass
     else:
-        print(f"{regkey_check_return[-1]}, {type(regkey_check_return[-1])}")
+        change_address(response)
 
-
-if sys.argv[1] == "-status" or sys.argv[1] == "-s":
-    status_check()
-elif sys.argv[1] == "-activate" or sys.argv[1] == "-a":
-    activate_proxy()
-elif sys.argv[1] == "-deactivate" or sys.argv[1] == "-d":
-    deactivate_proxy()
-elif sys.argv[1] == "-change" or sys.argv[1] == "-c":
-        if len(sys.argv) == 3:
-            change_address(sys.argv[2])
-        else :
-            print('Please enter a proxy address "-c newproxy:PORT"')
-elif sys.argv[1] == "-show" or sys.argv[1] == "-show":
-    print(fill_in())
-elif sys.argv[1] == "-help" or sys.argv[1] == "-h":
-    print("\n\n-status or -s: Check the current proxy status\n-activate or -a: Activate the proxy\n-deactivate or -d: Deactivate the proxy\n-change or -c: Change the proxy address (must specify the new address as an additional argument, e.g. setproxy -c newproxy:8080)\n-show: Show the current proxy address\nNote: If an invalid argument is provided, the program will display an error message.\n\n")
-else:
-    print("Wrong argument")
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")
+app = customtkinter.CTk()
+app.geometry("300x400")
+app.title("Proxy Manager")
+customtkinter.CTkButton(app, text="Change Proxy", command=button_click_event).place(
+    relx=0.05, rely=0.3, anchor=tkinter.NW
+)
+customtkinter.CTkButton(
+    master=app, text="Activate proxy", command=activate_proxy
+).place(relx=0.05, rely=0.10, anchor=tkinter.NW)
+customtkinter.CTkButton(
+    master=app, text="Deactivate proxy", command=deactivate_proxy
+).place(relx=0.05, rely=0.2, anchor=tkinter.NW)
+activeproxy = customtkinter.CTkLabel(master=app, text=check_active_proxy())
+activeproxy.place(relx=0.05, rely=0.4, anchor=tkinter.NW)
+app.mainloop()
